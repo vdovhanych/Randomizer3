3e9679f3b88ca700452c3a4b4165eb548fbd5906
Vitalij Dovhanyč <dovhanych@me.com>
2020-12-08 11:35:43 +0100

    Initial Commit

---
+++ Randomizer3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Randomizer3
+//
+//  Created by Vitalij Dovhanyč on 08.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Randomizer3/Randomizer3App.swift
@@ -0,0 +1,17 @@
+//
+//  Randomizer3App.swift
+//  Randomizer3
+//
+//  Created by Vitalij Dovhanyč on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct Randomizer3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
