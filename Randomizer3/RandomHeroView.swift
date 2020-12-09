5124ef61c3eaaa5fce51fbc68332a08722a8aa2f
Vitalij Dovhanyč <dovhanych@me.com>
2020-12-09 17:45:41 +0100

    Some minor changes and bug fixes

---
+++ Randomizer3/RandomHeroView.swift
@@ -0,0 +1,20 @@
+//
+//  RandomHeroView.swift
+//  Randomizer3
+//
+//  Created by Vitalij Dovhanyč on 09.12.2020.
+//
+
+import SwiftUI
+
+struct RandomHeroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RandomHeroView_Previews: PreviewProvider {
+    static var previews: some View {
+        RandomHeroView()
+    }
+}
